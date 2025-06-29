4ea6b3a623ff8082046e1fa5e3bafed1d29f5b09
esmat <eamuzain@gmail.com>
2025-06-29 16:06:45 -0400

    update s3

--- iac (1)/iac/s3.tf
+++ iac (1)/iac/s3.tf
@@ -1,15 +1,19 @@
-# create an s3 bucket 
+# Fetch current AWS account ID for unique bucket naming
+data "aws_caller_identity" "current" {}
+
+# Create an S3 bucket with unique name (appends AWS account ID)
 resource "aws_s3_bucket" "env_file_bucket" {
-  bucket = "${var.project_name}-${var.env_file_bucket_name}"
+  bucket = "${var.project_name}-${var.env_file_bucket_name}-${data.aws_caller_identity.current.account_id}"
 
   lifecycle {
     create_before_destroy = false
   }
 }
 
-# upload the environment file from local computer into the s3 bucket
+# Upload the environment file from local computer into the S3 bucket
 resource "aws_s3_object" "upload_env_file" {
   bucket = aws_s3_bucket.env_file_bucket.id
   key    = var.env_file_name
   source = "./${var.env_file_name}"
 }
+
